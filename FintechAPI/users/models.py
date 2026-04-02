834a8e1d825be35debd7f29e977d7ba313837b1d
AlokPy1484 <alokpandey1483@gmail.com>
2026-04-02 23:28:18 +0530

    initial commit
    * Setup and Config DONE
    * INCLUDES OpenAPI, DRF, CORS, JWT

---
+++ FintechAPI/users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        VIEWER = 'VIEWER', 'Viewer'
+        ANALYST = 'ANALYST', 'Analyst'
+        ADMIN = 'ADMIN', 'Admin'
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.VIEWER)
+
+    
+    def __str__(self):
+        return f"{self.username} ({self.role})"
